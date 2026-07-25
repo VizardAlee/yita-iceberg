0edaae400bc76cb45f3db9189db9556f7503fdda
serviceguru-crypt <serviceguru@svoltnigeria.com>
2026-07-25 16:56:17 +0100

    fix: update eslint.config.mjs for production App Check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
-    ".next-e2e*/**",
+    ".next-*/**",
     "out/**",
     "build/**",
     "coverage/**",
